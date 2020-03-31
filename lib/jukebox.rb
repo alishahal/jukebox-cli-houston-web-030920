5dd1a6b257303c92f486d03c881819d25926d5b2
Karim Macknojia <alishahal@msn.com>
2020-03-31 02:09:18 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,12 +1,4 @@
-# Add your code here
-# def say_hello(name)
-#     "Hi #{name}!"
-#   end
-   
-#   puts "Enter your name:"
-#   users_name = gets.strip
-   
-#   puts say_hello(users_name)
+ 
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -18,25 +10,46 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+
 def help 
-puts "I accept the following commands:"
+  
+puts  "I accept the following commands:"
 puts  "-help:display this help message"
 puts  "-list : displays a list of songs you can play"
 puts  "-play :lets you chose a song to play"
-puts "-exit : exits this program"
+puts  "-exit : exits this program"
+
 end 
 
-def play 
+def play(songs)
+  puts "Please enter a song name or number:"
+  users_input=gets.strip
+  if users_input 
+    puts "Playing <song name>"
+    
+  else 
+    puts "Invalid input, please try again"
 end 
 
 def list(songs)
-  songs.each_with_index do |val, index| 
+  songs.each_with_index do | val, index| 
     puts "#{index + 1}. #{val}"
   end
 end 
-list(songs)
+
+ 
+
 def exit_jukebox
+  puts "Goodbye!
 end 
 
 def run
+  help
+  puts "Please enter a command:"
+  users_input=gets.strip
+  while (users_input.down_case=!"exit") do
+  
+  
+  end
+  
 end 
